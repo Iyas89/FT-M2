c7c53a19cbe7a970c8cf97bab3d4e8b7e9b5f591
iyas89 <iyasasaad@gmail.com>
2023-08-17 14:19:03 -0300

    first commit

---
+++ 03-ES6/homework/src/selector.js
@@ -0,0 +1,46 @@
+var traverseDomAndCollectElements = function (matchFunc, startEl) {
+  var resultSet = [];
+
+  if (typeof startEl === "undefined") {
+    startEl = document.body;
+  }
+
+  // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
+  // usa matchFunc para identificar elementos que matchien
+
+  // TU CÓDIGO AQUÍ
+};
+
+// Detecta y devuelve el tipo de selector
+// devuelve uno de estos tipos: id, class, tag.class, tag
+
+var selectorTypeMatcher = function (selector) {
+  // tu código aquí
+  if (selector.includes("#")) return "id";
+  if (selector[0] === '.') return 'class';
+  if (selector.includes(".")) return 'tag.class';
+  else { return 'tag'}
+};
+
+// NOTA SOBRE LA FUNCIÓN MATCH
+// recuerda, la función matchFunction devuelta toma un elemento como un
+// parametro y devuelve true/false dependiendo si el elemento
+// matchea el selector.
+
+var matchFunctionMaker = function (selector) {
+  var selectorType = selectorTypeMatcher(selector);
+  var matchFunction;
+  if (selectorType === "id") {
+  } else if (selectorType === "class") {
+  } else if (selectorType === "tag.class") {
+  } else if (selectorType === "tag") {
+  }
+  return matchFunction;
+};
+
+var $ = function (selector) {
+  var elements;
+  var selectorMatchFunc = matchFunctionMaker(selector);
+  elements = traverseDomAndCollectElements(selectorMatchFunc);
+  return elements;
+};
